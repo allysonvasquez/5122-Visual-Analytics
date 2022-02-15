06169edeb7abb06500e849f6b2d5faf1b5e3acae
amill251 <alexmiller4481@gmail.com>
2022-02-14 23:10:32 -0500

    Removed comments

--- Homework-2/losses2015.py
+++ Homework-2/losses2015.py
@@ -17,29 +17,9 @@ if st.sidebar.checkbox('Show dataframe'):
     st.write(df)
 
 if st.sidebar.checkbox('Damage Result'):
-    df1 = df.groupby(['Damage_Descp']).mean('Amount').sort_values(by=['Amount'])
+    df1 = df.groupby(['Damage_Descp']).mean('Amount')
     st.bar_chart(df1)
 
 if st.sidebar.checkbox('States Amount'):
-    df1 = df.groupby(['State_Abv']).sum('Amount').sort_values(by=['Amount'])
+    df1 = df.groupby(['State_Abv']).sum('Amount')
     st.bar_chart(df1)
-
-# choose the sources of interest
-# cols = ['Coal_MW', 'Gas_MW', 'Hidroelectric_MW', 'Nuclear_MW', 'Wind_MW', 'Solar_MW', 'Biomass_MW']
-# option = st.multiselect('What sources do you want to display?', cols, cols[0])
-# option = st.multiselect('What sources do you want to display?', cols, cols[0])
-# cols = ['amount']
-# option = st.multiselect('What sources do you want to display?', cols, cols[0])
-
-
-# if st.sidebar.checkbox('Show Streamlit line_chart'):
-#     st.line_chart( df[option] )
-# else:
-#     fig, ax = plt.subplots()
-#     df[option].plot(ax=ax)
-#     st.write( fig )    
-
-# if st.sidebar.checkbox('Show mean daily production'):
-#     # assuming mean daily production is just reffering to the data point for each day?
-#     st.write( df[option])
-#     st.bar_chart(data=df[option], width=0, height=0, use_container_width=True)
